d9cb4929bd0f8300d5e240eaed807970a3d65280
hldogdng <hldogdng@yeah.net>
2019-08-01 22:20:07 +0800

    赛码的bug！cin要放到while中

--- job/股神.cpp
+++ job/股神.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 int main() {
 	int day;
-	while (1) {
+	while (cin >> day) {
 		int result = 1;
 		int increase = 1;
-		cin >> day;
 		int j = 1;
-		for (int i = day-1; i > 0; i--) {
+		for (int i = day - 1; i > 0; i--) {
 			if (j) {
 				result++;
 				j--;
@@ -20,7 +19,7 @@ int main() {
 				j = increase;
 			}
 		}
-		cout << result << endl;
+		printf("%d", result);
 	}
 	return 0;
 }
